abe775d7400f938f30f4216f3acc48cabb53f0e0
Mitchell Gerrard <mitchell.gerrard99@hotmail.com>
2021-10-20 23:29:03 +0100

    init

--- install_arch.sh
+++ install_arch.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 sudo pacman -Syu
-sudo pacman -S i3-wm i3status alacritty picom python feh ttf-font-awesome
+sudo pacman -S i3-wm i3status alacritty picom python feh ttf-font-awesome google-chrome rofi
 cd ~
 git clone https://github.com/mitchyman2011/dots.git
 cd Documents
 git clone https://github.com/mitchyman2011/University.git
 
 cd ~/Pictures
+git init
 git remote add origin https://github.com/mitchyman2011/Wallpapers.git
+
 cd ../dots
 ln -s ~/dots/i3 ~/.config/i3
 ln -s ~/dots/alacritty ~/.config/alacritty
