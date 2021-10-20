5a6f1aa3f4555da62570e2ba677e906844883c0e
Mitchell Gerrard <mitchell.gerrard99@hotmail.com>
2021-10-20 18:02:13 +0100

    installer for arch

---
+++ install_arch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+sudo pacman -S i3-wm i3status alacritty picom python
+cd ~
+git clone https://github.com/mitchyman2011/dots.git
+cd Documents
+git clone https://github.com/mitchyman2011/University.git
+
+cd /~/Pictures
+git remote add origin https://github.com/mitchyman2011/Wallpapers.git
+cd ../dots
+ln -s ~/dots/i3 ~/.config/i3
+ln -s ~/dots/alacritty ~/.config/alacritty
+ln -s ~/dots/picom ~/.config/picom
+cd ~/.config/i3
+git clone https://aur.archlinux.org/bumblebee-status.git
+cd bumblebee-status
+makepkg -sicr
+pip install --user bumblebee-status
+
