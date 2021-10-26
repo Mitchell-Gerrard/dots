e608f19a9fd4bdffd0e40ac77558ab4c1a015175
Mitchell Gerrard <mitchell.gerrard99@hotmail.com>
2021-10-26 01:11:41 +0100

    modified installer and added .bashrc

--- install_arch.sh
+++ install_arch.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 sudo pacman -Syu
-sudo pacman -S i3-wm i3status alacritty picom python feh ttf-font-awesome google-chrome rofi
-cd ~
-git clone https://github.com/mitchyman2011/dots.git
-cd Documents
-git clone https://github.com/mitchyman2011/University.git
+sudo pacman -S i3-wm i3status alacritty picom python python-pip feh ttf-font-awesome google-chrome rofi
 
 cd ~/Pictures
 git init
 git remote add origin https://github.com/mitchyman2011/Wallpapers.git
 git pull origin master
 cd ../dots
-ln -s ~/dots/i3 ~/.config/i3
-ln -s ~/dots/alacritty ~/.config/alacritty
-ln -s ~/dots/picom ~/.config/picom
+ln -sf ~/dots/i3 ~/.config/i3
+ln -sf ~/dots/alacritty ~/.config/alacritty
+ln -sf ~/dots/picom ~/.config/picom
+ln -sf ~/dots/.bashrc ~/.bashrc
 cd ~/.config/i3
 git clone https://aur.archlinux.org/bumblebee-status.git
 cd bumblebee-status
